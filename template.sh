66be44c305c5170e9b8774d07b482385211bf7f0
8604186911yash-ops <8604186911yash@gmail.com>
2026-02-10 15:11:16 +0530

    Add files via upload

---
+++ template.sh
@@ -0,0 +1,16 @@
+# creating directory
+mkdir  -p src
+mkdir  -p research
+
+# creating files 
+touch src/__inti__.py
+touch src/helper.py
+touch src/prompt.py 
+touch .env
+touch setup.py
+touch app.py
+touch reaseach/trails.ipynb
+touch requirements.txt
+
+
+echo    "DIRECTORY AND FILES CREATED SUCCESSFULLY!."
